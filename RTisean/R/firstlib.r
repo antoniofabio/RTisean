73a4cec901e139fb2be6c469d5e8ab34808dae5e
antonio <antonio>
2006-05-20 18:04:02 +0000

    fixed startup message

--- RTisean/R/firstlib.r
+++ RTisean/R/firstlib.r
@@ -2,7 +2,7 @@
 {
    library.dynam("RTisean",pkg,lib)
    .C("set_process_id",as.integer(Sys.getpid()),PACKAGE="RTisean")
-   cat("tiseanR 0.1-1 loaded and initialized\n")
+   cat("RTisean loaded and initialized\n")
 }
 
 
