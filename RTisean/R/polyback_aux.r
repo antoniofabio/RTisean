ef1b4a4a046caab59e2b5cbc24239a0c5093ba4b
antonio <antonio>
2006-05-22 09:50:31 +0000

    changed system call to R (cross-platform) call

--- RTisean/R/polyback_aux.r
+++ RTisean/R/polyback_aux.r
@@ -6,7 +6,7 @@ polyback_aux <- function(){
   name <- param_filename(1)
   suffixes <- find_suffixes(name)
   for (s in suffixes){
-    system(paste("mv ",param_filename(1),".",s," ",output_filename(),".",s,sep=""))
+		file.rename(paste(param_filename(1),".",s,sep=""),paste(output_filename(),".",s,sep=""))
     #should work on windows and linux 
   }
   suffixes <- find_suffixes(output_filename())
