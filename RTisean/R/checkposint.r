824a773f49cc603769820073fc32c3fc4f241d2b
antonio <antonio>
2006-05-28 14:56:15 +0000

    RT_predict fixed and well working

---
+++ RTisean/R/checkposint.r
@@ -0,0 +1,4 @@
+checkposint <- function(a){
+	if((round(a)==a)&&(a>0))	
+		return(TRUE)
+}
---
+++ RTisean/R/predict.r
@@ -0,0 +1,88 @@
+RT_predict <- function(series,d=-1,m=-1,r=-1,v=-1,s=1,l=-1,x=0,c=1){
+
+	options <- ""
+
+	if (mode(series)!="numeric")
+		stop("wrong input")
+	
+	if (l>0)
+			options <- paste(options," -l",l," ",sep="")	
+	
+	if (round(x)==x){
+		options <- paste(options," -x",i2s(x)," ",sep="")
+	}
+	else{
+		errormessage("x",x)
+		return()
+	}
+	
+	if ((round(c)==c)& (c>=1)) {
+		options=paste(options," -c",c," ",sep="")
+	} else{
+		errormessage("c",c)
+		return()
+	}
+	
+	if ((round(m)==m) && (m>0)){
+		options = paste(options," -m",i2s(m)," ",sep="")	
+	} 
+	else{
+		if (!tneq(m,-1)){
+			print("m value is not specified")
+		}
+		else{
+			errormessage("m",m)
+		}
+		return()
+	}
+	
+	if (checkposint(d)){
+		options = paste(options," -d",i2s(d)," ",sep="")	
+	}
+	else{
+		if (!tneq(d,-1)){
+			print("d value is not specified")
+		}
+		else{
+			errormessage("d",d)
+		}
+		return()
+	}
+	
+	if (tneq(v,-1) & tneq(r,-1))
+		stop("specifying both r and v is not allowed")
+	if ( !tneq(v,-1) & !tneq(r,-1) )
+		stop("either r or v has to be specified")
+	
+	if (tneq(r,-1)){
+		if (checkpositive(r)){
+			options = paste(options," -r",d2s(r)," ",sep="")	
+		}
+		else{ 	
+			errormessage("r",r)
+			return()
+		}
+	}
+	
+	if (tneq(v,-1)){
+		if (checkpositive(v)){
+			options = paste(options," -v",d2s(v)," ",sep="")	
+		}
+		else{ 	
+			errormessage("v",v)
+			return()
+		}
+	}
+	
+	
+	if (checkposint(s)){
+		options = paste(options, " -s",i2s(s)," ",sep="")
+	}
+	else{
+		errormessage("s",s)
+		return()
+	}
+	
+	out <- call_TISEANF_extended(series,options,"predict")
+	return(out[[1]][[1]])
+}
