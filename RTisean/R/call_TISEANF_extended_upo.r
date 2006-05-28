84d43bb3c6252ba25e92a5a394d94530972a4c21
antonio <antonio>
2006-05-28 07:38:45 +0000

    added forgotten files

---
+++ RTisean/R/call_TISEANF_extended_upo.r
@@ -0,0 +1,21 @@
+call_TISEANF_extended_upo <- function(a, bare_options, function_name) # a is a numeric vector, function_name is the TISEAN function name
+{
+    out=write_to_inputfile(a)
+    if (out==1){
+	print("wrong input")
+        return()
+    }
+    tin=input_filename()
+    tout=output_filename()
+    options=paste(tin," ",bare_options," -V0 -o",tout,"\n", sep="")
+    
+	toptions=param_filename(0)
+	.C("write_string_to_file2",as.character(options),as.integer(0),as.character(toptions),PACKAGE="RTisean")
+	.Fortran(function_name,as.character(toptions),PACKAGE="RTisean")
+ 	txt <- readLines(tout, n = -1, ok = TRUE)
+	file.remove(toptions, tin)
+	out <- read_TISEAN("")
+	attr(out, "txt") <- txt
+	return(out)
+}
+
