4506ee663d69a36913740589256ab4439563aa2f
antonio <antonio>
2006-06-05 07:57:35 +0000

    fixed nstep, upgraded docs

---
+++ RTisean/R/nstep.r
@@ -0,0 +1,18 @@
+nstep <- function(series,l,x=0,m,c,d=1,L=1000,k=30,r,f=1.2,O=FALSE){
+	concat <- get("c",envir=as.environment("package:base"))
+	args <- list(routinename="nstep", input=series, x=x,d=d,L=L,k=k,f=f)
+	if(O)
+		args <- concat(args, list("O"))
+	if(!missing(l))
+		args <- concat(args, l=l)
+	if(!missing(m))
+		args <- concat(args, m=m)
+	if(!missing(c))
+		args <- concat(args, m=m)
+	if(!missing(r))
+		args <- concat(args, r=r)
+	if(missing(c)) {c <- get("c",envir=as.environment("package:base"))}
+	out <- as.matrix(do.call(callTISEAN, args))
+	colnames(out) <- "predictions"
+	return(out)
+}
