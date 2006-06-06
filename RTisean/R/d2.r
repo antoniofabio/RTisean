28fedd43164be84c5c7b7ff26257ebd6e36d99d2
antonio <antonio>
2006-06-06 09:06:34 +0000

    renamed firstlib.r to conventional zzz.R, fixed typo in d2.r

--- RTisean/R/d2.r
+++ RTisean/R/d2.r
@@ -1,4 +1,4 @@
-d2 <- function(series,l,x=0,d=1,M,c,t=0,R=-1,r=-1,scale=100,N=1000,E=FALSE){
+d2 <- function(series,l,x=0,d=1,M,c,t=0,R,r,scale=100,N=1000,E=FALSE){
 	concat <- get("c",envir=as.environment("package:base"))
 	args <- list()
 	if(!missing(l))
@@ -7,9 +7,13 @@ d2 <- function(series,l,x=0,d=1,M,c,t=0,R=-1,r=-1,scale=100,N=1000,E=FALSE){
 		args <- concat(args, list("E"))
 	if(!missing(M))
 		args <- concat(args, list(M=M))
+	if(!missing(R))
+		args <- concat(args, list(R=R))
+	if(!missing(r))
+		args <- concat(args, list(r=r))
 	if(!missing(c))
 		args <- concat(args, list(c=c))
-	args <- concat(args, list(routinename="d2",input=series, x=x, d=d, t=t, R=R, r=r, "#"=scale, N=N, 
+	args <- concat(args, list(routinename="d2",input=series, x=x, d=d, t=t, "#"=scale, N=N, 
 		suffixes=concat(".c2",".d2",".h2",".stat")))
 	if(missing(c)) {c <- get("c",envir=as.environment("package:base"))}
 	out <- do.call(callTISEAN, args)[1:3]
