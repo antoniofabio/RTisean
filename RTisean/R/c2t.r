be2ede6e461402c61d4628338761b9e98aaee446
antonio <antonio>
2006-06-06 07:54:23 +0000

    fixed mistake in c2t, upgraded doc

--- RTisean/R/c2t.r
+++ RTisean/R/c2t.r
@@ -3,9 +3,9 @@ c2t <- function(lst){
 		stop("wrong input")
 	class(lst) <- c("TISEANmatrixlist","list") #needs special serialization method
 
-	out <- as.list(callTISEAN("c2g",input=lst))
+	out <- as.list(callTISEAN("c2t",input=lst))
 	out <- as.matrixList(out)
-	out <- colnamesout(out,1,c("epsilon","corr.integral", "log.der.")) #FIXME: what represents those 3 columns?
+	out <- colnamesout(out,1,c("epsilon","log.der."))
 	nl <- floor(length(out)/2)
 	nms <- unlist(out[(1:nl-1)*2+1])
 	out <- out[(1:length(nms))*2]
