a43e83f34095e6cc9aaef51b55ada500b2094ce3
antonio <antonio>
2007-03-07 14:07:56 +0000

    fixed pca interface code and docs to the new 3.0 TISEAN version

--- RTisean/R/pca.r
+++ RTisean/R/pca.r
@@ -1,13 +1,10 @@
-RT_pca <- function(series,l,x=0,c=1,m=2,d=1,q){
-	args <- list(routinename="pca", input=series, x=x, c=c, m=m, d=d)
+RT_pca <- function(series,l,x=0,c=1,m=c(1,2),d=1, W=0, q){
+	args <- list(routinename="pca", input=series, x=x, c=c, m=m, d=d, W=W)
 	if(!missing(l))
 		args <- c(args, l=l)
 	if(!missing(q))
 		args <- c(args, q=q)
-	out <- as.list(do.call(callTISEAN, args))
-	eig <- as.numeric(gsub(".* +(.*)","\\1",out[[1]]))
-	ans <- list(eigen=eig, project=as.matrix(out[[2]]))
-	return(ans)
+	return(as.matrix(do.call(callTISEAN, args)))
 }
 
 RT_svn <- RT_pca
