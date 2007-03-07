f08c1bc4532b3b698e421ff9060e93eee59bc3ea
antonio <antonio>
2007-03-07 14:21:45 +0000

    fixed (?) lfo.test routine to new TISEAN-3.0.0 version

--- RTisean/R/lfo-test.r
+++ RTisean/R/lfo-test.r
@@ -1,7 +1,7 @@
 #Output has to be explicitely redirected to a temp file
-lfo.test <- function(series,l,x=0,c,m=2,d=1,n,k=30,r,f=1.2,s=1,C){
+lfo.test <- function(series,l,x=0,c=1,m=c(1,2),d=1,n,k=30,r,f=1.2,s=1,C){
 	concat <- get("c",envir=as.environment("package:base"))
-	args <- list(routinename="lfo.test", input=series, x=x, m=m, d=d,k=k,f=f,s=s)
+	args <- list(routinename="lfo-test", input=series, x=x, m=m, d=d,k=k,f=f,s=s)
 	if(!missing(l))
 		args <- concat(args, l=l)
 	if(!missing(c))
@@ -14,8 +14,9 @@ lfo.test <- function(series,l,x=0,c,m=2,d=1,n,k=30,r,f=1.2,s=1,C){
 		args <- concat(args, C=C)
 	if(missing(c)) {c <- get("c",envir=as.environment("package:base"))}
 	args <- c(args, noout=TRUE)
-	out <- as.character(do.call(callTISEAN,args))
-	out <- as.numeric(gsub(".*= (.*)","\\1",out))
+	out <- as.character(do.call(callTISEAN,args))[2]
+	#out <- as.numeric(gsub(".*= (.*)","\\1",out))
+	out <- as.numeric(out)
 	return(out)
 }
 
