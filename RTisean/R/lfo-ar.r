5540ccf6624fe8437beef0a277288986092dee88
antonio <antonio>
2007-03-08 08:38:50 +0000

    renamed ll_ar to lfo.ar to reflect TISEAN-3.0.0 changes. Upgraded changelog and DESCRIPTION date

--- RTisean/R/lfo-ar.r
+++ RTisean/R/lfo-ar.r
@@ -1,5 +1,5 @@
-ll_ar <- function(series,l,x=0,c=1,m=2,d=1,i,r,R,f=1.2,s=1,C){
-	args <- list(routinename="ll-ar", input=series, x=x,c=c,m=m,d=d,f=f,s=s)
+lfo.ar <- function(series,l,x=0,c=1,m=c(1,2), d=1,i,r,R,f=1.2,s=1,C){
+	args <- list(routinename="lfo-ar", input=series, x=x,c=c,m=m,d=d,f=f,s=s)
 	if(!missing(l))
 		args <- c(args, l=l)
 	if(!missing(i))
@@ -15,3 +15,5 @@ ll_ar <- function(series,l,x=0,c=1,m=2,d=1,i,r,R,f=1.2,s=1,C){
 	colnames(out) <- c("neigh.size","rel.error", "fraction", "avg.neigh.", "variance" )
 	return(out)
 }
+
+ll_ar <- lfo.ar
